9c67870026650166c4bb7d233e470f8f226df233
John Brayton <jbrayton@goldenhillsoftware.com>
2023-01-08 18:46:09 -0500

    Initial Commit

---
+++ Icons/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Icons
+//
+//  Created by John Brayton on 1/8/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
